c14833541e0d2fe7e3f0003559ba2f2d9f3df8c4
feng <764523295@qq.com>
2018-05-31 17:28:18 +0800

    udpate

---
+++ demo.go
@@ -0,0 +1,39 @@
+package main
+
+import  "fmt"
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+
+
+type t struct{
+    b string
+}
+
+func (b *t) Test (a string) string{
+    return b.b + a
+}
+
+func main() {
+    var bb t
+    bb.b = "sadfsadf"
+    fmt.Println(bb.b)
+    var ab = bb.Test("bbbbb")
+    fmt.Println(ab)
+
+    db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    res, err := db.Exec("INSERT INTO userinfo (username, departname, created) VALUES (?, ?, ?)","lily","销售","2016-06-21")
+    CheckErr(err)
+
+    fmt.Println(res)
+
+}
+
+func CheckErr(err error){
+    if err != nil {
+        fmt.Println(err)
+    }
+}
