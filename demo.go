76dc275acdccc7423602d094bd65b8a553cac9da
feng <123@qq.com>
2022-06-04 12:29:59 +0800

    升级go1.18

--- demo.go
+++ demo.go
@@ -1,86 +1,90 @@
 package main
 
 import (
-	"time"
 	"bufio"
-	"io/ioutil"
-	"io"
-	"os"
-	"fmt"
 	"database/sql"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/axgle/mahonia"
 	_ "github.com/go-sql-driver/mysql"
-    "github.com/gomodule/redigo/redis"
-    "github.com/mahonia"
+	"github.com/gomodule/redigo/redis"
 )
 
-
-type t struct{
-    b string
+type t struct {
+	b string
 }
 
-func (b *t) Test (a string) string{
-    return b.b + a
+func (b *t) Test(a string) string {
+	return b.b + a
 }
 
 func main() {
-    //test1()
-    //write_file()
-    //read_file()
-    //test_chan()
-    //test2()
-    test3()
+	//test1()
+	//write_file()
+	//read_file()
+	//test_chan()
+	//test2()
+	test3()
 }
 
 func test3() {
-    done := make(chan struct{})
-    langs := []string{"Go", "C", "C++", "Java", "Perl", "Python"}
-    for _, l := range langs { go warrior(l, done) }
-    //for _ = range langs { <-done }
+	done := make(chan struct{})
+	langs := []string{"Go", "C", "C++", "Java", "Perl", "Python"}
+	for _, l := range langs {
+		go warrior(l, done)
+	}
+	//for _ = range langs { <-done }
 }
+
 var battle = make(chan string)
+
 func warrior(name string, done chan struct{}) {
-    defer fmt.Println("bbbb")
-    select {
-    case opponent := <-battle:
-        fmt.Printf("%s beat %s\n", name, opponent)
-    case battle <- name:
-        // I lost :-(
-    }
-    done <- struct{}{}
+	defer fmt.Println("bbbb")
+	select {
+	case opponent := <-battle:
+		fmt.Printf("%s beat %s\n", name, opponent)
+	case battle <- name:
+		// I lost :-(
+	}
+	done <- struct{}{}
 }
 
 func test2() {
-    block, done := make(chan struct{}), make(chan struct{})
-    for i := 0; i < 4; i++ {
-        go waiter(i, block, done)
-    }
-    time.Sleep(5 * time.Second)
-    close(block)
-    for i := 0; i < 4; i++ {
-        <-done
-    }
+	block, done := make(chan struct{}), make(chan struct{})
+	for i := 0; i < 4; i++ {
+		go waiter(i, block, done)
+	}
+	time.Sleep(5 * time.Second)
+	close(block)
+	for i := 0; i < 4; i++ {
+		<-done
+	}
 }
 func waiter(i int, block, done chan struct{}) {
-    time.Sleep(1e9)
-    fmt.Println(i, "waiting...")
-    <-block
-    fmt.Println(i, "done!")
-    done <- struct{}{}
+	time.Sleep(1e9)
+	fmt.Println(i, "waiting...")
+	<-block
+	fmt.Println(i, "done!")
+	done <- struct{}{}
 }
 
-func test_chan(){
-    ch := make(chan int)
-    tesc_chan_in(ch)
-    tesc_chan_out(ch)
+func test_chan() {
+	ch := make(chan int)
+	tesc_chan_in(ch)
+	tesc_chan_out(ch)
 
-    time.Sleep(1e9)
+	time.Sleep(1e9)
 }
-func tesc_chan_in(c chan<- int){
-    c <- 123
+func tesc_chan_in(c chan<- int) {
+	c <- 123
 }
-func tesc_chan_out(c <-chan int){
-    v := <-c
-    fmt.Println(v)
+func tesc_chan_out(c <-chan int) {
+	v := <-c
+	fmt.Println(v)
 }
 
 func check(e error) {
@@ -90,25 +94,25 @@ func check(e error) {
 }
 
 func write_file() {
-    var writeString = "测试nnnn"
-    var filename = "./output1.txt"
-    var f *os.File
-    var err1 error
+	var writeString = "测试nnnn"
+	var filename = "./output1.txt"
+	var f *os.File
+	var err1 error
 
-    /*****************第一种方法 使用 io.WriteString 写入文件 *********************/
-    if checkFileIsExist(filename) { //如果文件存在
-        f, err1 = os.OpenFile(filename, os.O_APPEND, 0666)  //打开文件
-        fmt.Println("文件存在")
-    } else {
-        f, err1 = os.Create(filename) //创建文件
-        fmt.Println("文件不存在已创建")
-    }
-    CheckErr(err1)
-    n, err1 := io.WriteString(f, writeString)   //写入文件，字符串
-    CheckErr(err1)
-    fmt.Printf("写入 %d 个字符n", n)
+	/*****************第一种方法 使用 io.WriteString 写入文件 *********************/
+	if checkFileIsExist(filename) { //如果文件存在
+		f, err1 = os.OpenFile(filename, os.O_APPEND, 0666) //打开文件
+		fmt.Println("文件存在")
+	} else {
+		f, err1 = os.Create(filename) //创建文件
+		fmt.Println("文件不存在已创建")
+	}
+	CheckErr(err1)
+	n, err1 := io.WriteString(f, writeString) //写入文件，字符串
+	CheckErr(err1)
+	fmt.Printf("写入 %d 个字符n", n)
 
-    /*****************************  第二种方式: 使用 ioutil.WriteFile 写入文件 ***********************************************/
+	/*****************************  第二种方式: 使用 ioutil.WriteFile 写入文件 ***********************************************/
 	var d1 = []byte(writeString)
 	err2 := ioutil.WriteFile("./output2.txt", d1, 0666) //写入文件(字节数组)
 	check(err2)
@@ -133,70 +137,70 @@ func write_file() {
 }
 
 func checkFileIsExist(filename string) bool {
-    var exist = true
-    if _, err := os.Stat(filename); os.IsNotExist(err) {
-        exist = false
-    }
-    return exist
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
 }
 
 func read_file() {
-    file, err := os.Open("./test.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
+	file, err := os.Open("./test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    //文件ex7.txt的编码是gb18030
-    decoder := mahonia.NewDecoder("utf8")
-    if decoder == nil {
-        fmt.Println("编码不存在")
-    }
-    buf := make([]byte, 1024)
-    for {
-        len, _ := file.Read(buf)
+	//文件ex7.txt的编码是gb18030
+	decoder := mahonia.NewDecoder("utf8")
+	if decoder == nil {
+		fmt.Println("编码不存在")
+	}
+	buf := make([]byte, 1024)
+	for {
+		len, _ := file.Read(buf)
 
-        if len == 0 {
-            break
-        }
+		if len == 0 {
+			break
+		}
 
-        fmt.Println(string(buf))
-    }
-    file.Close()
+		fmt.Println(string(buf))
+	}
+	file.Close()
 }
 
-func test1(){
-    var bb t
-    bb.b = "sadfsadf"
-    fmt.Println(bb.b)
-    var ab = bb.Test("bbbbb")
-    fmt.Println(ab)
+func test1() {
+	var bb t
+	bb.b = "sadfsadf"
+	fmt.Println(bb.b)
+	var ab = bb.Test("bbbbb")
+	fmt.Println(ab)
 
-    db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8")
-    if err != nil {
-        fmt.Println(err)
-    }
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    _, err2 := db.Exec("INSERT INTO userinfo (username, departname, created) VALUES (?, ?, ?)","lily","销售","2016-06-21")
-    CheckErr(err2)
+	_, err2 := db.Exec("INSERT INTO userinfo (username, departname, created) VALUES (?, ?, ?)", "lily", "销售", "2016-06-21")
+	CheckErr(err2)
 
-    c, err := redis.Dial("tcp", "127.0.0.1:6379")
-    CheckErr(err)
+	c, err := redis.Dial("tcp", "127.0.0.1:6379")
+	CheckErr(err)
 
-    defer c.Close()
+	defer c.Close()
 
-    _, err = c.Do("SET", "mykey", "superWang")
-    CheckErr(err)
+	_, err = c.Do("SET", "mykey", "superWang")
+	CheckErr(err)
 
-    username, err := redis.String(c.Do("GET", "mykey"))
-    if err != nil {
-        fmt.Println("redis get failed:", err)
-    } else {
-        fmt.Printf("Get mykey: %v \n", username)
-    }
+	username, err := redis.String(c.Do("GET", "mykey"))
+	if err != nil {
+		fmt.Println("redis get failed:", err)
+	} else {
+		fmt.Printf("Get mykey: %v \n", username)
+	}
 }
 
-func CheckErr(err error){
-    if err != nil {
-        fmt.Println(err)
-    }
+func CheckErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
---
+++ shop.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/monaco-io/request"
+)
+
+func main() {
+	fmt.Println("shop tools")
+	itemid := "10727937149"
+	keyword := "book"
+	limit := 100
+	newest := 0
+	maxNewest := 0 //PC60一页
+	url := "https://th.xiapibuy.com/api/v4/search/search_items?by=relevancy&keyword=" + keyword + "&limit=" + strconv.Itoa(limit) + "&newest=" + strconv.Itoa(newest) + "&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2"
+
+	for i := newest; i <= maxNewest; i = i + limit {
+		time.Sleep(5)
+		if handleContent(url, itemid, newest, keyword) {
+			break
+		}
+	}
+
+}
+
+func handleContent(url string, itemid string, newest int, keyword string) bool {
+	var result interface{}
+	param := md5.Sum([]byte("by=relevancy&keyword=" + keyword + "&limit=100&newest=" + strconv.Itoa(newest) + "&order=desc&page_type=search&version=2"))
+	paramMd5 := string(fmt.Sprintf("%x", param))
+	s1 := md5.Sum([]byte("55b03" + paramMd5 + "55b03"))
+	k := string(fmt.Sprintf("%x", s1))
+
+	fmt.Println(k)
+
+	// $header  = array(
+	// 	'if-none-match-: 55b03-'.$k,
+	// 	'user-agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
+	// );
+
+	c := request.Client{
+		URL:    url,
+		Method: "GET",
+		Header: map[string]string{"if-none-match-": "55b03-" + k, "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"},
+	}
+	resp := c.Send().Scan(&result)
+	if !resp.OK() {
+		// handle error
+		fmt.Println(resp.Error())
+	}
+	res, _ := json.Marshal(result)
+	strData := string(res)
+	// fmt.Println(strData)
+	isContain := strings.Contains(strData, itemid)
+	if isContain {
+		regex := regexp.MustCompile(`"itemid":\d+,"label`)
+		arr := regex.FindAllString(strData, -1)
+		rank := 0
+		for i, v := range arr {
+			if strings.Contains(v, itemid) {
+				rank = newest + i + 1
+				fmt.Println("排名：", rank)
+			}
+		}
+		fmt.Println("https://th.xiapibuy.com/search?keyword=book&page=" + strconv.Itoa((rank-1)/60+1))
+		return true
+	}
+	return false
+}
