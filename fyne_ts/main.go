2384c75c7de292ffb90c16130cd570269732ae6e
qgw@qq.com <qgw@qq.com>
2022-09-26 19:23:27 +0800

    fyne

---
+++ fyne_ts/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+// go build -ldflags -H=windowsgui
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+	w.Resize(fyne.NewSize(400, 200))
+	w.ShowAndRun()
+}
